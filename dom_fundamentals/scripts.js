00d85ca3a638c0fe7a5b77a7245031b9138b3997
Kanthamas <114124443+Kanthamas@users.noreply.github.com>
2024-05-15 01:51:43 +0700

    add dom_fundamentals

---
+++ dom_fundamentals/scripts.js
@@ -0,0 +1,74 @@
+// DOM Manipulation
+
+// Step 1: Access Elements
+// getElementById --> That Element
+// getElementsByClassName --> HTMLCollection
+// getElementsByTagName --> HTMLCollection
+// querySelector --> First Element
+// querySelectorAll --> NodeList --> .forEach()
+
+// Step 2A: Manipulate Elements
+// Step 2B: Respond to Events:
+
+function changeHeading() {
+	// const mainHeadingEl = document.getElementById("main-heading");
+	// console.log(mainHeadingEl);
+
+	const mainHeadingEl = document.querySelector("#main-heading");
+
+	mainHeadingEl.innerHTML = `<span>Hello JS DOM 😎</span>`;
+	mainHeadingEl.style.fontSize = "3rem";
+	mainHeadingEl.style.fontWeight = "bold";
+	mainHeadingEl.style.color = "red";
+}
+
+function changeParagraphs() {
+	const contentEl = document.getElementsByClassName("content");
+	// console.log(contentEl);
+	const contents = Array.from(contentEl);
+
+	// const paragraphs = document.getElementsByTagName("p");
+	// console.log(paragraphs);
+	// let contents = Array.from(paragraphs);
+
+	// const contents = document.querySelectorAll(".content");
+	// console.log(contents);
+
+	contents.forEach((paragraph, index) => {
+		paragraph.textContent = `This is paragraph ${index + 1}`;
+		paragraph.classList.add("font-bold");
+		paragraph.classList.add("text-2xl");
+		paragraph.classList.add("text-orange-700");
+		paragraph.classList.add("py-4");
+	});
+}
+
+// handle magic button (2 ways of coding)
+
+// 1. call function method
+/* function handleMagicBtn() {
+	const magicBtnEl = document.querySelector("#magic-btn");
+	magicBtnEl.addEventListener("click", changeHeading);
+}
+handleMagicBtn(); */
+
+// 2. onClick method
+function magicBtnClick() {
+	const magicBtnEl = document.querySelector("#magic-btn");
+
+	changeHeading();
+
+	// changeParagraphs();
+}
+
+// handle reset button by relaoding page
+function reloadPage() {
+	// reload the current page
+	window.location.reload();
+}
+
+function resetBtnClick() {
+	const resetBtnEl = document.querySelector("#reset-btn");
+	resetBtnEl.addEventListener("click", reloadPage);
+}
+resetBtnClick();
